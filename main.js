2d2c8edcd2879b7fdd7a91c103d16cb95b0811e1
Ben Singer <benjamin.singer@helpsystems.com>
2015-01-29 11:00:46 -0600

    random games are created now

--- main.js
+++ main.js
@@ -33,27 +33,8 @@ var SquareView = Backbone.Marionette.ItemView.extend({
     }
 });
 
-var BoardView = Backbone.Marionette.CompositeView.extend({
-	tagname: 'div',
-	className: 'boardContainer',
-	template: '#board',
-	childView: SquareView,
-	childViewContainer: '.board',	
-	events : {
-        'click': 'checkForWin'
-    },
-    checkForWin: function(ev){
-    	if(window.msg !== 'win'){
-	    	if(this.validateBoard() === true){
-	    		window.msg = "win";
-	    		this.render();
-	    	}
-    	}
-    },
-    validateBoard : function(){    
-    	var a = this.collection.toJSON();	    	
-    	return this.collection.toJSON(a) && this.validateSamesies(a) && this.validateEqualColors(a) && this.validateThrees(a);    	
-    },
+var validationObj = {
+	
     validateSamesies: function(a){
     	var h = [];
 		for(var i = 0; i < 4; i++){
@@ -149,6 +130,29 @@ var BoardView = Backbone.Marionette.CompositeView.extend({
     	}
     	return true;
     }
+};
+
+var BoardView = Backbone.Marionette.CompositeView.extend({
+	tagname: 'div',
+	className: 'boardContainer',
+	template: '#board',
+	childView: SquareView,
+	childViewContainer: '.board',	
+	events : {
+        'click': 'checkForWin'
+    },
+    checkForWin: function(ev){
+    	if(window.msg !== 'win'){
+	    	if(this.validateBoard() === true){
+	    		window.msg = "win";
+	    		this.render();
+	    	}
+    	}
+    },
+    validateBoard : function(){    
+    	var a = this.collection.toJSON();	   
+    	return validationObj.validateSamesies(a) && validationObj.validateEqualColors(a) && validationObj.validateThrees(a);    	
+    }   
 });
 
 App.addRegions({
@@ -156,12 +160,78 @@ App.addRegions({
 });
 
 window.msg = '';
-var game = new Board([
-	new Square({ v: 0, locked: false }), new Square({ v: 1, locked: true }), new Square({ v: 0, locked: false }), new Square({ v: 0, locked: false }), 
-	new Square({ v: 0, locked: false }), new Square({ v: 0, locked: false }), new Square({ v: 2, locked: true }), new Square({ v: 0, locked: false }),
-	new Square({ v: 0, locked: false }), new Square({ v: 0, locked: false }), new Square({ v: 0, locked: false }), new Square({ v: 0, locked: false }),
-	new Square({ v: 0, locked: false }), new Square({ v: 0, locked: false }), new Square({ v: 0, locked: false }), new Square({ v: 2, locked: true })
-]);
+
+
+var randomNum = function(min, max){
+    return Math.floor(Math.random()*(max-min+1)+min);
+}
+
+var possibleRows = [1122, 1212, 1221, 2211, 2121, 2112];
+//get first row
+var index = randomNum(0, 5)
+var row1 = possibleRows[index];
+possibleRows.splice(index, 1);
+//get second row
+index = randomNum(0, 4)
+var row2 = possibleRows[index];
+possibleRows.splice(index, 1);
+
+var tempBoard = (row1 + "").split('').map(function(s){ return parseInt(s, 10) });
+tempBoard = tempBoard.concat((row2 + "").split('').map(function(s){ return parseInt(s, 10) }));
+
+var boardFound = false;
+for(var i = 0; i < possibleRows.length; i++){
+	tempBoard.splice(8, tempBoard.length);
+	var row3 = possibleRows[i];
+	tempBoard = tempBoard.concat((row3 + "").split('').map(function(s){ return parseInt(s, 10) }));
+	if(i !== j){
+		for(var j = 0; j < possibleRows.length; j++){
+			tempBoard.splice(12, tempBoard.length);
+			var row4 = possibleRows[j];
+			tempBoard = tempBoard.concat((row4 + "").split('').map(function(s){ return parseInt(s, 10) }));			
+			
+			var a = tempBoard.map(function(v){ return { v: v } });
+			if(validationObj.validateSamesies(a) && validationObj.validateEqualColors(a) && validationObj.validateThrees(a)){
+				boardFound = true;
+				break;
+			}
+		}
+	}
+	if(boardFound){
+		break;
+	}
+}
+
+var lockedIndexes = [];
+for(var i = 0; i < randomNum(3, 4); i++){
+	var go = true;
+	while(go){
+		var rtemp = randomNum(0, 15);
+		if(lockedIndexes.indexOf(rtemp) < 0){
+			lockedIndexes.push(rtemp);
+			go = false;
+		}
+	}	
+}
+
+var board = [];
+for(var i = 0; i < tempBoard.length; i++){
+	if(lockedIndexes.indexOf(i) < 0){
+		tempBoard[i] = 0;
+		board[i] = new Square({ v: 0, locked: false });
+	}else{
+		board[i] = new Square({ v: tempBoard[i], locked: true });
+	}
+}
+
+var game = new Board(board);
+
+// var game = new Board([
+// 	new Square({ v: 0, locked: false }), new Square({ v: 1, locked: true }), new Square({ v: 0, locked: false }), new Square({ v: 0, locked: false }), 
+// 	new Square({ v: 0, locked: false }), new Square({ v: 0, locked: false }), new Square({ v: 2, locked: true }), new Square({ v: 0, locked: false }),
+// 	new Square({ v: 0, locked: false }), new Square({ v: 0, locked: false }), new Square({ v: 0, locked: false }), new Square({ v: 0, locked: false }),
+// 	new Square({ v: 0, locked: false }), new Square({ v: 0, locked: false }), new Square({ v: 0, locked: false }), new Square({ v: 2, locked: true })
+// ]);
 
 App.page.show(new BoardView({ collection: game }));
 	
